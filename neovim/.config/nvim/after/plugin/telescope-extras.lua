72df27cb2f337f99646fe166e94ce91d99e2a680
Micael Viana <micael.vianasilva@gmail.com>
2025-03-05 15:37:45 -0400

    nvim - put frecency keymaps in the same file than Telescope config

--- neovim/.config/nvim/after/plugin/telescope-extras.lua
+++
@@ -1,3 +0,0 @@
--- Eu nao estava conseguindo fazer isso funcionar no outro arquivo
-vim.keymap.set("n", "<space>p",":Telescope frecency workspace=CWD <cr>", {desc="Frecency"})
-vim.keymap.set("n", "tf", ":Telescope frecency workspace=CWD <cr>", {})
--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,11 @@
+-- lua/plugins/telescope.lua
 return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-media-files.nvim",
-      {
-          "nvim-telescope/telescope-frecency.nvim",
-          version = "^0.9.0",
-      },
+      "nvim-telescope/telescope-frecency.nvim",
       "nvim-tree/nvim-web-devicons", -- Para ícones
     },
     config = function()
@@ -43,8 +41,17 @@ return {
       telescope.load_extension("media_files")
       telescope.load_extension("frecency")
 
-      -- Mapeamentos de teclas
+      -- Função para abrir o frecency com workspace=CWD
+      local open_frecency = function()
+        require("telescope").extensions.frecency.frecency({
+          workspace = "CWD", -- Define o workspace como o diretório atual
+        })
+      end
+
       local builtin = require("telescope.builtin")
+      -- Mapeamentos de teclas
+      vim.keymap.set("n", "<space>p", open_frecency, {}) -- Usar frecency para <space>p
+      vim.keymap.set("n", "tf", open_frecency, {}) -- Usar frecency para tf
       vim.keymap.set("n", "tb", builtin.builtin, {})
       vim.keymap.set("n", "tr", builtin.oldfiles, {})
       vim.keymap.set("n", "tg", builtin.live_grep, {})
