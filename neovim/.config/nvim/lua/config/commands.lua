7d51139bcde360834f15d8911dce40916380e5b3
Micael Viana <micael.vianasilva@gmail.com>
2025-03-06 15:17:13 -0400

    nvim - add keymap to sanitize the Windows clipboard

--- neovim/.config/nvim/lua/config/commands.lua
+++ neovim/.config/nvim/lua/config/commands.lua
@@ -1,1 +1,2 @@
 vim.api.nvim_create_user_command('WindowsCleaning','%s/\r/' , {})
+vim.keymap.set('n','<space>c','<cmd>WindowsCleaning<cr>',{noremap=true,silent=true,desc='Fix Windows weird clipboard behaviour'})
