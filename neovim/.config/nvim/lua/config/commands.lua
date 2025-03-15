927acfbb9cd8e9f1a45e3ddce6e60894cdfadf1c
Micael Viana <micael.vianasilva@gmail.com>
2025-03-15 18:25:25 -0400

    nvim - remap WindowsCleaning

--- neovim/.config/nvim/lua/config/commands.lua
+++ neovim/.config/nvim/lua/config/commands.lua
@@ -1,2 +1,2 @@
 vim.api.nvim_create_user_command('WindowsCleaning','%s/\r/' , {})
-vim.keymap.set('n','<space>cl','<cmd>WindowsCleaning<cr>',{noremap=true,silent=true,desc='Fix Windows weird clipboard behaviour'})
+vim.keymap.set('n','<space>cw','<cmd>WindowsCleaning<cr>',{noremap=true,silent=true,desc='Fix Windows weird clipboard behaviour'})
