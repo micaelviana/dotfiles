009a6c9900f60a9b5aff5f7278a971535cce2b66
Micael Viana <micael.vianasilva@gmail.com>
2025-03-21 13:17:57 -0400

    nvim - enable quickfile and bigfile

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -7,14 +7,14 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    bigfile = { enabled = false },
+    bigfile = { enabled = true },
     dashboard = { enabled = false },
     explorer = { enabled = false },
     indent = { enabled = false },
     input = { enabled = false },
     picker = { enabled = true },
     notifier = { enabled = false },
-    quickfile = { enabled = false },
+    quickfile = { enabled = true },
     scope = { enabled = false },
     scroll = { enabled = false },
     statuscolumn = { enabled = false },
