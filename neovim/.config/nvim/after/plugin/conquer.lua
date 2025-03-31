f8c5b87e5d60fb98d083640336f6f9efb6e52e55
Micael Viana <micael.vianasilva@gmail.com>
2025-03-31 11:32:09 -0400

    nvim: change coc keymap to navigate between diagnostics

--- neovim/.config/nvim/after/plugin/conquer.lua
+++ neovim/.config/nvim/after/plugin/conquer.lua
@@ -41,8 +41,8 @@ keyset("i", "<c-space>", "coc#refresh()", { silent = true, expr = true })
 
 -- Use `[g` and `]g` to navigate diagnostics
 -- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list
-keyset("n", "[g", "<Plug>(coc-diagnostic-prev)", { silent = true })
-keyset("n", "]g", "<Plug>(coc-diagnostic-next)", { silent = true })
+keyset("n", "[d", "<Plug>(coc-diagnostic-prev)", { silent = true })
+keyset("n", "]d", "<Plug>(coc-diagnostic-next)", { silent = true })
 
 -- GoTo code navigation
 keyset("n", "gd", "<Plug>(coc-definition)", { silent = true })
