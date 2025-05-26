c6182b3e50d6b1688483423887f6b1eb19eca5eb
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-05-26 16:26:54 -0400

    nvim: add boole lua

---
+++ neovim/.config/nvim/lua/plugins/boole.lua
@@ -0,0 +1,13 @@
+return {
+  'nat-418/boole.nvim',
+  event = 'BufEnter',
+  opts = {
+    mappings = {
+      increment = '<C-a>',
+      decrement = '<C-x>',
+      -- true
+      -- no
+      -- 6
+    },
+  },
+}
