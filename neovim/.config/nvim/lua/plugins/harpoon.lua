8010913c2bb4f611a7488439a0369629fcce0342
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-06-18 19:58:55 -0400

    nvim - update harpoon config

--- neovim/.config/nvim/lua/plugins/harpoon.lua
+++ neovim/.config/nvim/lua/plugins/harpoon.lua
@@ -8,8 +8,8 @@ return {
       require("harpoon"):setup()
     end,
     keys = {
-      -- Keybindings for Harpoon
-      { "<leader>ha", function() require("harpoon"):list():add() end, desc = "Harpoon: Append file to list" },
+     -- Keybindings for Harpoon
+      { "<leader>hh", function() require("harpoon"):list():add() end, desc = "Harpoon: Append file to list" },
       { "<leader>he", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Harpoon: Toggle quick menu" },
       { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "Harpoon: Go to file 1" },
       { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "Harpoon: Go to file 2" },
