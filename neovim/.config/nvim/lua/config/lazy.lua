3e03ee9211771dea00b992795a555a8dd34980c2
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-06-23 12:04:29 -0400

    nvim - set tokyonight as the main theme

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -15,8 +15,8 @@ rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-local colorscheme_repo = "EdenEast/nightfox.nvim"
-local colorscheme_name = "dayfox"
+local colorscheme_repo = "folke/tokyonight.nvim"
+local colorscheme_name = "tokyonight-night"
 require("lazy").setup({
 	change_detection = { notify = false },
 	checker = {
