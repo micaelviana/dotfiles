6eca56278cd07a2e22331fc49189f9f004d240b0
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-07-07 15:13:12 -0400

    nvim - add snacks rename function

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -1,30 +1,49 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    bigfile = { enabled = true },
-    explorer = {
-      enabled=false
-    },
-    quickfile = { enabled = true },
-    dashboard = { enabled = false },
-    indent = { enabled = false },
-    input = { enabled = false },
-    picker = { enabled = false },
-    notifier = { enabled = false },
-    scope = { enabled = false },
-    scroll = { enabled = false },
-    statuscolumn = { enabled = false },
-    words = { enabled = false },
-  },
-   keys = {
-    --Other
-    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-    { "<leader>bo", function() Snacks.bufdelete.other() end, desc = "Delete all buffers except the current one"},
-  }
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = true },
+		explorer = {
+			enabled = false,
+		},
+		quickfile = { enabled = true },
+		dashboard = { enabled = false },
+		indent = { enabled = false },
+		input = { enabled = false },
+		picker = { enabled = false },
+		notifier = { enabled = false },
+		scope = { enabled = false },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+	},
+	keys = {
+		--Other
+		{
+			"<leader>bd",
+			function()
+				Snacks.bufdelete()
+			end,
+			desc = "Delete Buffer",
+		},
+		{
+			"<leader>bo",
+			function()
+				Snacks.bufdelete.other()
+			end,
+			desc = "Delete all buffers except the current one",
+		},
+		{
+			"<leader>rf",
+			function()
+				Snacks.rename.rename_file()
+			end,
+			desc = "Rename File",
+		},
+	},
 }
