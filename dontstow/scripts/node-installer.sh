1de20b653b1bcf8ca9d2f24ab0e5a31c03634738
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-08-01 19:37:09 -0400

    scripts - update nvm version

--- dontstow/scripts/node-installer.sh
+++ dontstow/scripts/node-installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #download
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 #export variable
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
