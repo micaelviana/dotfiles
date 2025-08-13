2b112b13487316aa651c2e34ee6082f803fdcc06
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-08-13 12:21:12 -0400

    nvim: keep just changes to the defaults of nvim-ts-autotag

--- neovim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ neovim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -3,9 +3,6 @@ return {
 	lazy = false,
 	opts = {
 		opts = {
-			-- Defaults
-			enable_close = true, -- Auto close tags
-			enable_rename = true, -- Auto rename pairs of tags
 			--It is not a default
 			enable_close_on_slash = true, -- Auto close on trailing </
 		},
