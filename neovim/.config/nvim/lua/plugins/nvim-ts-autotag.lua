04dfaf14b27bef9b7067b919aebff80f73168f4b
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-08-13 12:20:12 -0400

    nvim: enable close on slash for html files

--- neovim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ neovim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,4 +1,13 @@
 return {
 	"windwp/nvim-ts-autotag",
 	lazy = false,
+	opts = {
+		opts = {
+			-- Defaults
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			--It is not a default
+			enable_close_on_slash = true, -- Auto close on trailing </
+		},
+	},
 }
