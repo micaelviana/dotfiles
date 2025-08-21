33f3e0ad180d8db2098fc1857779a3e2dcbb58b7
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-08-21 12:20:27 -0400

    nvim: update nvim scissors config

--- neovim/.config/nvim/after/plugin/nvim-scissors-config.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set("n", "<leader>se", function()
-	require("scissors").editSnippet()
-end, { desc = "Snippet: Edit" })
-
--- when used in visual mode, prefills the selection as snippet body
-vim.keymap.set({ "n", "x" }, "<leader>sa", function()
-	require("scissors").addNewSnippet()
-end, { desc = "Snippet: Add" })
--- neovim/.config/nvim/lua/plugins/nvim-scissors.lua
+++ neovim/.config/nvim/lua/plugins/nvim-scissors.lua
@@ -1,6 +1,24 @@
+--WARNING: hot reload doesn't work if the .json file was not created before
 return {
 	"chrisgrieser/nvim-scissors",
 	opts = {
 		snippetDir = "~/.config/nvim/snippets/",
+	},
+	keys = {
+		{
+			"<leader>se",
+			function()
+				require("scissors").editSnippet()
+			end,
+			desc = "Snippet: Edit",
+		},
+		{
+			"<leader>sa",
+			function()
+				require("scissors").addNewSnippet()
+			end,
+			mode = { "n", "x" },
+			desc = "Snippet: Add",
+		},
 	},
 }
