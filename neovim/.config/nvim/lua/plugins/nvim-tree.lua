4860a5c83a4ab3ea3f5233a1e479d8c478c2d979
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-08-21 14:30:07 -0400

    nvim: add autocmd to nvim-tree integration with snacks

--- neovim/.config/nvim/lua/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -30,5 +30,20 @@ return {
 
 		-- optionally enable 24-bit colour
 		vim.opt.termguicolors = true
+
+		--Integration between Snacks rename file function and NvimTree
+		local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "NvimTreeSetup",
+			callback = function()
+				local events = require("nvim-tree.api").events
+				events.subscribe(events.Event.NodeRenamed, function(data)
+					if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
+						data = data
+						Snacks.rename.on_rename_file(data.old_name, data.new_name)
+					end
+				end)
+			end,
+		})
 	end,
 }
