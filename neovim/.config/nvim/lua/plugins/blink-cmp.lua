5a608228f64e7ba4f53fbbe1c947bf8243615234
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-09-04 09:23:11 -0400

    nvim: disable autoselect completion

--- neovim/.config/nvim/lua/plugins/blink-cmp.lua
+++ neovim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -44,6 +44,11 @@ return { -- Autocompletion
 			-- By default, you may press `<c-space>` to show the documentation.
 			-- Optionally, set `auto_show = true` to show the documentation after a delay.
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+			list = {
+				selection = {
+					preselect = false, -- Disable preselecting the first item
+				},
+			},
 		},
 
 		sources = {
