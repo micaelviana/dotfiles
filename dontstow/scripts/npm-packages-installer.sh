6ad066bb87dcad9b9f863ed54c823ba5ffea16ce
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-09-06 13:42:59 -0400

    scripts: remove some unused npm packages

--- dontstow/scripts/npm-packages-installer.sh
+++ dontstow/scripts/npm-packages-installer.sh
@@ -5,6 +5,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 npm i -g bash-language-server
-npm i -g neovim
-npm i -g tree-sitter
 npm i -g live-server
