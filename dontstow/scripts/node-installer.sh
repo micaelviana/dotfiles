147a9e90182d16c4aaad639d22d3f34cd7e3e966
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-09-14 21:16:12 -0400

    scripts: add comamnd to install latest npm

--- dontstow/scripts/node-installer.sh
+++ dontstow/scripts/node-installer.sh
@@ -7,3 +7,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 #install node
 nvm install --lts
+nvm install-latest-npm
