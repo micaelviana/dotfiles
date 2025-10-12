72dcffda5ede883d01a88e6a1ec05654eed0e36b
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-10-12 12:32:47 -0400

    nvim: move gotofile command as poc

--- neovim/.config/nvim/lua/config/commands.lua
+++ neovim/.config/nvim/lua/config/commands.lua
@@ -1,7 +1,12 @@
-vim.api.nvim_create_user_command('FormatClipboard','%s/\r/' , {})
-vim.keymap.set('n','<space>t', --format clipboard
-function ()
- vim.cmd('FormatClipboard')
- vim.cmd([[normal! '']])
-end,
-{noremap=true,silent=true,desc='Fix Windows weird clipboard behaviour'})
+vim.api.nvim_create_user_command("FormatClipboard", "%s/\r/", {})
+vim.keymap.set(
+	"n",
+	"<space>t", --format clipboard
+	function()
+		vim.cmd("FormatClipboard")
+		vim.cmd([[normal! '']])
+	end,
+	{ noremap = true, silent = true, desc = "Fix Windows weird clipboard behaviour" }
+)
+
+vim.api.nvim_create_user_command("GoToFile", 'lua require("fzf-lua").files()', {})
--- neovim/.config/nvim/lua/plugins/fzf.lua
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -1,22 +1,22 @@
-return{
-  lazy=false,
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys={
-    {'<space>B','<cmd>FzfLua builtin<cr>', desc='List all fzf commands'},
-    { "<space>f", '<cmd>FzfLua files<cr>', desc = "Find Files" },
-    { "tg", '<cmd>FzfLua live_grep<cr>', desc = "Grep" },
-    { "to", '<cmd>FzfLua oldfiles<cr>', desc = "Old files" },
-    { "th", '<cmd>FzfLua helptags<cr>', desc = "Help pages" },
-    { "tk", '<cmd>FzfLua keymaps<cr>', desc = "Keymaps" },
-    { "tb", '<cmd>FzfLua buffers<cr>', desc = "Buffers" },
-  },
-  config = function ()
-    --Select a file using Fuzzy finder
-    vim.api.nvim_create_user_command('GoToFile','lua require("fzf-lua").files()',{})
-    --prevent cursor for blinking
-    vim.opt.guicursor:remove { 't:block-blinkon500-blinkoff500-TermCursor' }
-  end,
-  opts = {}
+return {
+	lazy = false,
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{ "<space>B", "<cmd>FzfLua builtin<cr>", desc = "List all fzf commands" },
+		{ "<space>f", "<cmd>FzfLua files<cr>", desc = "Find Files" },
+		{ "tg", "<cmd>FzfLua live_grep<cr>", desc = "Grep" },
+		{ "to", "<cmd>FzfLua oldfiles<cr>", desc = "Old files" },
+		{ "th", "<cmd>FzfLua helptags<cr>", desc = "Help pages" },
+		{ "tk", "<cmd>FzfLua keymaps<cr>", desc = "Keymaps" },
+		{ "tb", "<cmd>FzfLua buffers<cr>", desc = "Buffers" },
+	},
+	config = function()
+		--Select a file using Fuzzy finder
+		-- vim.api.nvim_create_user_command('GoToFile','lua require("fzf-lua").files()',{})
+		--prevent cursor for blinking
+		vim.opt.guicursor:remove({ "t:block-blinkon500-blinkoff500-TermCursor" })
+	end,
+	opts = {},
 }
