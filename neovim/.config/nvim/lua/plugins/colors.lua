756d63242231460fad21bd3248021b2d3e54e5f5
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-10-28 08:19:28 -0400

    nvim: set nordfox as the main theme

---
+++ neovim/.config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,18 @@
+return {
+	--collection
+	{ "sainnhe/everforest", name = "everforest", lazy = true },
+	{ "EdenEast/nightfox.nvim", lazy = true }, --collection
+	{ "sainnhe/sonokai", lazy = true },
+	{ "folke/tokyonight.nvim", lazy = true },
+	{ "bluz71/vim-nightfly-colors", lazy = true },
+
+	--selected one
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("nightfox")
+		end,
+	},
+}
