a715489ed73afe2d6a184fe82b1feb4bc7fd8da7
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-11-17 09:11:30 -0400

    nvim: set rose pine as the main theme

--- neovim/.config/nvim/lua/plugins/colors.lua
+++ neovim/.config/nvim/lua/plugins/colors.lua
@@ -3,17 +3,17 @@ return {
 	{ "sainnhe/everforest", name = "everforest", lazy = true },
 	{ "sainnhe/sonokai", lazy = true },
 	{ "folke/tokyonight.nvim", lazy = true },
+	{ "rose-pine/neovim", name = "rose-pine", lazy = true },
 
 	--selected one
 	{
-		"sainnhe/everforest",
+		"rose-pine/neovim",
 		lazy = false,
 		priority = 1000,
 		config = function()
 			local bg = os.getenv("BACKGROUND") or "dark"
 			vim.opt.background = bg
-			vim.g.everforest_enable_italic = true
-			vim.cmd.colorscheme("everforest")
+			vim.cmd.colorscheme("rose-pine")
 		end,
 	},
 }
