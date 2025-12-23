12b12638b9101a45fcc825e170f78e3b3d4901ea
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2025-12-23 13:31:04 -0400

    nvim: add autocmd to automatic create folders

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -18,6 +18,16 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufEnter" }, {
 	command = "set filetype=html",
 })
 
+-- Create directories when saving files
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		local dir = vim.fn.expand("<afile>:p:h")
+		if vim.fn.isdirectory(dir) == 0 then
+			vim.fn.mkdir(dir, "p")
+		end
+	end,
+})
+
 -- LSP-integrated file renaming for Oil
 vim.api.nvim_create_autocmd("User", {
 	pattern = "OilActionsPost",
