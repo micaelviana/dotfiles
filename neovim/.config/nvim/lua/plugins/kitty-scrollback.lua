575b7af23ff56f097c350331e0365dc0f4b9c7e3
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2026-02-12 16:29:26 -0400

    nvim: add config to quit with y

--- neovim/.config/nvim/lua/plugins/kitty-scrollback.lua
+++ neovim/.config/nvim/lua/plugins/kitty-scrollback.lua
@@ -11,4 +11,13 @@ return {
 	-- version = '*', -- latest stable version, may have breaking changes if major version changed
 	-- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
 	opts = {},
+	-- NOTE: kitty-navigator doesn't quit after yanking to system clipboard, but if I try to enable this config the "yy" command will not going to work anymore
+	config = function()
+		vim.g.simpler_scrollback = vim.env.SIMPLER_SCROLLBACK or "default"
+		if vim.g.simpler_scrollback == "kitty-scrollback" then
+			-- yank and quit when using my scrollback config
+			vim.keymap.set({ "n", "v" }, "y", [["+y<cmd>q!<cr>]], { desc = "[P]Yank to system clipboard + Quit" })
+			-- vim.keymap.set({ "n", "v" }, "q", "<cmd>q!<cr>", { desc = "[P]Quit" })
+		end
+	end,
 }
