16ddf7cc1243f21628c08c3d33fb1c1a1f670d88
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2026-08-29 17:33:18 -0400

    kitty: add scrollback

--- .config/nvim/lua/plugins/kitty-scrollback.lua
+++ .config/nvim/lua/plugins/kitty-scrollback.lua
@@ -1,7 +1,7 @@
 return {
 	"mikesmithgh/kitty-scrollback.nvim",
 	enabled = true,
-	lazy = false,
+	lazy = true,
 	cmd = {
 		"KittyScrollbackGenerateKittens",
 		"KittyScrollbackCheckHealth",
@@ -10,15 +10,7 @@ return {
 	event = { "User KittyScrollbackLaunch" },
 	-- version = '*', -- latest stable version, may have breaking changes if major version changed
 	-- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-	opts = {},
-	-- NOTE: kitty-scrollback doesn't quit after yanking to system clipboard, but if I try to enable this config the "yy" command will not going to work anymore
 	config = function()
-		vim.g.simpler_scrollback = vim.env.SIMPLER_SCROLLBACK or "default"
-		if vim.g.simpler_scrollback == "kitty-scrollback" then
-			-- yank and quit when using my scrollback config
-			vim.keymap.set({ "n", "v" }, "y", [["+y<cmd>q!<cr>]], { desc = "[P]Yank to system clipboard + Quit" })
-			--this is already defined in the kitty config file
-			-- vim.keymap.set({ "n", "v" }, "q", "<cmd>q!<cr>", { desc = "[P]Quit" })
-		end
+		require("kitty-scrollback").setup()
 	end,
 }
